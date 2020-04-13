c718fb3f00a0279e5e46ac2120d0cfca67001389
Zane and Meredith <sorensenzane@gmail.com>
2020-04-13 13:48:36 -0500

    Initial Commit

---
+++ closet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  closet
+//
+//  Created by U-High EngineeringTech on 4/13/20.
+//  Copyright © 2020 University High School STEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
